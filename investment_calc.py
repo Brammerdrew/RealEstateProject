a7f01a35cc1d38446152f61799182bcd350fe43b
Brammerdrew <141886230+Brammerdrew@users.noreply.github.com>
2023-11-14 23:31:17 -0500

    added inputs

--- investment_calc.py
+++ investment_calc.py
@@ -1,41 +1,89 @@
-class rentalProperty:
-    def __init__(self, rental_income, misc_income, piti_expenses, maintenance_expenses, misc_expenses, down_payment, rehab_costs):
-        self.rental_income = rental_income
-        self.misc_income = misc_income
-        self.piti_expenses = piti_expenses
-        self.maintenance_expenses = maintenance_expenses
-        self.misc_expenses = misc_expenses
+class RentalProperty:
+    def __init__(self, purchase_price, down_payment, closing_costs, rehab_budget, monthly_rent, mortgage_rate, mortgage_term, vacancy,
+                 property_tax, property_management, insurance, maintenance, cap_ex, rental_income,
+                 laundry_income, storage_income, misc_income, mortgage_payment):
+        self.purchase_price = purchase_price
         self.down_payment = down_payment
-        self.rehab_costs = rehab_costs
+        self.closing_costs = closing_costs
+        self.rehab_budget = rehab_budget
+        self.monthly_rent = monthly_rent
+        self.mortgage_rate = mortgage_rate
+        self.mortgage_term = mortgage_term
+        self.vacancy = vacancy
+        self.property_tax = property_tax
+        self.property_management = property_management
+        self.insurance = insurance
+        self.maintenance = maintenance
+        self.cap_ex = cap_ex
+        self.rental_income = rental_income
+        self.laundry_income = laundry_income
+        self.storage_income = storage_income
+        self.misc_income = misc_income
+        self.mortgage_payment = mortgage_payment
+
+    def monthly_expenses(self):
+        return self.property_tax + self.property_management + self.insurance + self.maintenance + self.cap_ex + self.mortgage_payment
+
+    def monthly_income(self):
+        return self.rental_income + self.laundry_income + self.storage_income + self.misc_income
+
+    def upfront_costs(self):
+        return self.purchase_price + self.down_payment + self.closing_costs + self.rehab_budget
+
+    def yearly_income(self):
+        return self.monthly_income() * 12
+
+    def yearly_expenses(self):
+        return self.monthly_expenses() * 12
+
+    def cash_flow(self):
+        return self.yearly_income() - self.yearly_expenses()
+
+    def calculate_roi(self):
+        return (self.cash_flow() / self.upfront_costs()) * 100  # Return ROI as a percentage
 
 
-    def total_income(self):
-        return (self.rental_income + self.misc_income) * 12
-    
-    def total_expenses(self):
-        return (self.piti_expenses + self.maintenance_expenses + self.misc_expenses) * 12
+def main():
+    purchase_price = float(input("What is the purchase price? "))
+    down_payment = float(input("What is the down payment? "))
+    closing_costs = float(input("What are the closing costs? "))
+    rehab_budget = float(input("What is the rehab budget? "))
+    monthly_rent = float(input("What is the monthly rent? "))
+    mortgage_rate = float(input("What is the mortgage rate? "))
+    mortgage_term = float(input("What is the mortgage term? "))
+    vacancy = float(input("What is the vacancy rate? "))
+    property_tax = float(input("What is the property tax? "))
+    property_management = float(input("What is the property management fee? "))
+    insurance = float(input("What is the insurance cost? "))
+    maintenance = float(input("What is the maintenance cost? "))
+    cap_ex = float(input("What is the capital expenditure cost? "))
+    rental_income = float(input("What is the rental income? "))
+    laundry_income = float(input("What is the laundry income? "))
+    storage_income = float(input("What is the storage income? "))
+    misc_income = float(input("What is the misc income? "))
+    mortgage_payment = float(input("What is the mortgage payment? ")) 
+
+    rental_property = RentalProperty(
+        purchase_price, down_payment, closing_costs, rehab_budget, monthly_rent, mortgage_rate, mortgage_term, vacancy,
+        property_tax, property_management, insurance, maintenance, cap_ex, rental_income,
+        laundry_income, storage_income, misc_income, mortgage_payment
+    )
+
+    print("The monthly expenses are: ${:.2f}".format(rental_property.monthly_expenses()))
+    print("The monthly income is: ${:.2f}".format(rental_property.monthly_income()))
+    print("The upfront costs are: ${:.2f}".format(rental_property.upfront_costs()))
+    print("The yearly income is: ${:.2f}".format(rental_property.yearly_income()))
+    print("The yearly expenses are: ${:.2f}".format(rental_property.yearly_expenses()))
+    print("The cash flow is: ${:.2f}".format(rental_property.cash_flow()))
+    print("The ROI is: {:.2f}%".format(rental_property.calculate_roi()))
+
+
+if __name__ == "__main__":
+    main()
+
+
         
-    def calc_net_income(self):
-        return self.total_income() - self.total_expenses()
-    
-    def upfront_investment(self):
-        return self.rehab_costs - self.down_payment
-    
 
-class ROI:
-    def __init__(self, property):
-        self.property = property
-
-    def calc_roi(self):
-        net_income = self.property.calc_net_income()
-        total_investment = self.property.upfront_investment()
-        roi = (net_income / total_investment) * 100 
-        return roi
-    
-test_property = rentalProperty(2400,0,1150,250,250,4500,20000)
-roi_calculater = ROI(test_property)
-roi = roi_calculater.calc_roi()
-print(roi)
 
 
         
